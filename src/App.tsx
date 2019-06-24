038cf74698e1d9f834d0765b75671439a9f8c54e
Håkon Harnes <haakoeha@stud.ntnu.no>
2019-06-24 13:42:21 +0200

    Boilerplate cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>GiEffektivt admin panel is alive!</p>
       </header>
     </div>
