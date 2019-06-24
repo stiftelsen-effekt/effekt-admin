a5110fa20bf83a191c632f2f2e7269ef5be9a0ba
Håkon Harnes <haakoeha@stud.ntnu.no>
2019-06-24 15:07:28 +0200

    Adds basic redux store and reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import './App.css';
 
+import { createStore, Action } from 'redux';
+import { AppState } from './models/state'
+
+const initialState: AppState = {
+  count: 0
+}
+
+const reducer = (state: AppState = initialState, action: Action<any>) => {
+  console.log("Reducer", state, action);
+  return state;
+}
+const store = createStore(reducer);
+
 const App: React.FC = () => {
   return (
     <div className="App">
