39cc47ab79742b0fd154c8297b1676babd6a5271
Håkon Harnes <haakoeha@stud.ntnu.no>
2019-07-11 16:35:32 +0200

    Hooks up payment method fetching to component

--- src/components/app/modules/single-donation/single-donation.component.tsx
+++ src/components/app/modules/single-donation/single-donation.component.tsx
@@ -6,11 +6,25 @@ import { EffektSelect } from "../../style/elements/select.style"
 
 import { EffektDatePicker } from '../../style/elements/datepicker.style';
 import "react-datepicker/dist/react-datepicker.css";
+import { IPaymentMethod } from '../../../../models/dbtypes';
+import { useSelector, useDispatch } from 'react-redux';
+import { AppState } from '../../../../models/state';
+import { fetchPaymentMethodsRequest } from './single-donation.actions';
 
 export const SingleDonation: React.FunctionComponent = (props) => {
     const [startDate, setStartDate] = useState<Date | null>(new Date())
 
-    
+    const dispatch = useDispatch()
+    const paymentMethods = useSelector<AppState, Array<IPaymentMethod> | undefined>((state: AppState) => state.singleDonation.paymentMethods)
+
+    let paymentOptions = null;
+    if (!paymentMethods) dispatch(fetchPaymentMethodsRequest())
+    else {
+        paymentOptions = paymentMethods.map((method, i) => (
+            <option key={i} value={method.id}>{method.name}</option>
+        ));
+    }
+
 
     return (
         <SingleDonationWrapper>
@@ -28,7 +42,9 @@ export const SingleDonation: React.FunctionComponent = (props) => {
                 <EffektText placeholder="Sum" style={{ width: '90px' }}></EffektText>
                 <EffektText placeholder="Repeat sum" style={{ width: '90px' }}></EffektText>
                 <EffektText placeholder="External ref." style={{ width: '90px' }}></EffektText>
-                <EffektSelect placeholder="Payment method" style={{ width: '90px' }}></EffektSelect>
+                <EffektSelect placeholder="Payment method" style={{ width: '90px' }}>
+                    {paymentOptions}
+                </EffektSelect>
             </InputWrapper>
             <KIDComponent></KIDComponent>
         </SingleDonationWrapper>
