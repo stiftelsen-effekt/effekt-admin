77f56fcbee4c3460fc4578486be39354c7f000c0
Håkon Harnes <haakoeha@stud.ntnu.no>
2019-07-25 15:17:06 +0200

    Adds effekt checkbox form

---
+++ src/components/app/style/elements/effekt-check/effekt-check-form.component.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { EffektCheckFormWrapper } from './effekt-check-form.component.style';
+import { EffektCheck } from './effekt-check.component';
+
+export interface EffektCheckChoice {
+    label: string,
+    value: any,
+    selected: boolean
+}
+
+interface IProps {
+    choices: Array<EffektCheckChoice>,
+    onChange(selectedValues: Array<any>): void
+}
+
+export const EffektCheckForm: React.FunctionComponent<IProps> = ({ choices, onChange }) => {
+    let checkBoxes = choices.map((choice, index) => <EffektCheck key={index} label={choice.label} checked={choice.selected} onChange={(checked) => {
+        let updatedValues: Array<any> = choices.reduce((result: Array<any>, origChoice: EffektCheckChoice) => {
+            if (choice.label === origChoice.label) {
+                if (checked) {
+                    result.push(choice.value);
+                    return result
+                }
+                else {
+                    return result
+                }
+            }
+            if (origChoice.selected) {
+                result.push(origChoice.value)
+            }
+            return result
+        }, [])
+        onChange(updatedValues)
+    }}></EffektCheck>)
+    return (
+        <EffektCheckFormWrapper>
+            {checkBoxes}
+        </EffektCheckFormWrapper>
+    )    
+}
