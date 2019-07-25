0867f91c329fe2ac66cd1c590e80689d6b688172
Håkon Harnes <haakoeha@stud.ntnu.no>
2019-07-25 15:17:30 +0200

    Adds styled submit input

---
+++ src/components/app/style/elements/submit.style.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import { brown50 } from "../colors";
+
+export const EffektSubmit = styled.input`
+    padding: 10px 30px;
+    
+    font-family: 'Roboto';
+    font-weight: 500;
+    letter-spacing: 1px;
+    font-size: 14px;
+    border: none;
+    cursor: pointer;
+    box-shadow: 0 1px 3px 0px rgba(0,0,0,.2);
+    background: ${brown50};
+    color: white;
+`
