455c6867d60dc821b20ab6cd71e094ec01764315
Håkon Harnes <haakoeha@stud.ntnu.no>
2019-07-25 15:15:55 +0200

    Adds corect light gray color

--- src/components/app/style/colors.ts
+++ src/components/app/style/colors.ts
@@ -10,6 +10,7 @@ export const brown05 = "#90836F"
 export const grey30 = "#8A8A8A"
 export const grey15 = "#B7B7B7"
 export const grey05 = "#E2E2E2"
+export const grey02 = "#F4F4F4"
 
 export const red30 = "#4D0000"
 export const red20 = "#ad0000"
