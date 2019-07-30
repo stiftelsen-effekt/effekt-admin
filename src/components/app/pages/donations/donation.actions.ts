5a9df6dbe53b5034df32019f3394202941669f1b
Håkon Harnes <haakoeha@stud.ntnu.no>
2019-07-30 14:49:47 +0200

    Removes unneeded file

--- src/components/app/pages/donations/donation.actions.ts
+++
@@ -1,6 +0,0 @@
-import actionCreatorFactory from "typescript-fsa";
-import { IDonation } from "../../../../models/types";
-
-const actionCreator = actionCreatorFactory();
-
-export const fetchDonationAction = actionCreator.async<{id: number}, IDonation, Error>('FETCH_DONATION');
