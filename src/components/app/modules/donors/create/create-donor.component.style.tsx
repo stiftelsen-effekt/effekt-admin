8f02e051fa190b591803d680b1fa98f779bfacfd
Håkon Harnes <haakoeha@stud.ntnu.no>
2019-08-01 16:55:19 +0200

    Adds max-width to donor create component

--- src/components/app/modules/donors/create/create-donor.component.style.tsx
+++ src/components/app/modules/donors/create/create-donor.component.style.tsx
@@ -3,8 +3,9 @@ import styled from "styled-components";
 export const CreateDonorWrapper = styled.div`
     display: flex;
     flex-direction: column;
+    max-width: 200px;
     
     & > * {
-        margin: 10px 0;
+        margin: 6px 0;
     }
 `
