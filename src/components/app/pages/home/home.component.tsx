566bcf3f7ba03a1323249f0492afbb6b00f48458
Håkon Harnes <haakoeha@stud.ntnu.no>
2019-12-23 16:31:50 +0100

    Style update for scorecard

--- src/components/app/pages/home/home.component.tsx
+++ src/components/app/pages/home/home.component.tsx
@@ -7,7 +7,17 @@ export const HomeComponent: React.FunctionComponent = (props) => {
         <Page>
             <MainHeader>Home</MainHeader>
             <div>
-                <iframe title="Scorecard" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTHfRf7Zzh3YUSKs2g2Mim6GkRYR5yRUuoPQa1b61Ul4y1dxEGVempdlYpwte0yrahyvoqqWrbOxAkC/pubhtml?gid=880180721&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+                <iframe 
+                    style={
+                        {
+                            maxWidth: '1024px',
+                            width: '100%',
+                            height: '1800px'
+                        }
+                    } 
+                    title="Scorecard" 
+                    src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTHfRf7Zzh3YUSKs2g2Mim6GkRYR5yRUuoPQa1b61Ul4y1dxEGVempdlYpwte0yrahyvoqqWrbOxAkC/pubhtml?gid=880180721&amp;single=true&amp;widget=true&amp;headers=false"> 
+                    </iframe>
             </div>
         </Page>
     )
