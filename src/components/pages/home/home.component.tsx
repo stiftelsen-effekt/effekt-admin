b6d8ae2905a840cc9ddd499642ee39fe57503e3b
Håkon Harnes <hh@eiendomsverdi.no>
2020-03-31 14:10:21 +0200

    Updates scorecard

--- src/components/pages/home/home.component.tsx
+++ src/components/pages/home/home.component.tsx
@@ -8,16 +8,11 @@ export const HomeComponent: React.FunctionComponent = (props) => {
             <MainHeader>Home</MainHeader>
             <div>
                 <iframe 
-                    style={
-                        {
-                            maxWidth: '1024px',
-                            width: '100%',
-                            height: '1800px'
-                        }
-                    } 
-                    title="Scorecard" 
-                    src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTHfRf7Zzh3YUSKs2g2Mim6GkRYR5yRUuoPQa1b61Ul4y1dxEGVempdlYpwte0yrahyvoqqWrbOxAkC/pubhtml?gid=880180721&amp;single=true&amp;widget=true&amp;headers=false"> 
-                    </iframe>
+                    width="1100" 
+                    height="3500"
+                    title="Scorecard"
+                    src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS6Wiw_6vcspIHq6J2byvqmlDFadG5sF4cFQvwhm2z21ZYbfBW0tQknixlu2hV6lDi_TV16iwUYcJ3W/pubhtml?gid=880180721&amp;single=true&amp;widget=true&amp;headers=false&amp;scrolling=no">
+                </iframe>
             </div>
         </Page>
     )
