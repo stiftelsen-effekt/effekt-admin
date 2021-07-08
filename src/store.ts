4ab75208547213f09e4fc3f9024c303074fa9396
Philip <44257880+philiphand@users.noreply.github.com>
2021-07-08 09:56:17 +0200

    Uncomments redux devtools

--- src/store.ts
+++ src/store.ts
@@ -6,7 +6,7 @@ import { authReducer } from './authenticate/auth.reducer';
 import { donorSelectorReducer } from './components/modules/donors/selection/donor-selection.reducer';
 import { organizationsReducer } from './store/organizations/organizations.reducer';
 import { singleDonationReducer } from './components/modules/single-donation/single-donation.reducer';
-// import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { reportProcessingReducer } from './components/pages/process/process.reducer';
 import { CreateDonorReducer } from './components/modules/donors/create/create-donor.reducer';
 import { donationsReducer } from './store/donations/donations.reducer';
@@ -37,8 +37,8 @@ const rootReducer = combineReducers<AppState>({
 const sagaMiddleware = createSagaMiddleware();
 const Store = createStore(
     rootReducer,
-    applyMiddleware(sagaMiddleware));
-    //composeWithDevTools(applyMiddleware(sagaMiddleware)));
+    // applyMiddleware(sagaMiddleware));
+    composeWithDevTools(applyMiddleware(sagaMiddleware)));
 sagaMiddleware.run(watchAll);
 
 export default Store;
