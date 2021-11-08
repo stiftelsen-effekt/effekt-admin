68cf647808c94f50f3850d42396c2512589c11fa
Philip <44257880+philiphand@users.noreply.github.com>
2021-11-08 10:25:13 +0100

    Removed temporary dev config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,5 @@
 export const DEV_ENVIRONMENT: boolean = process.env.NODE_ENV === 'development' ? true : false;
-//export const API_URL: string = false ? 'http://localhost:3000' : 'https://data.gieffektivt.no';
-export const API_URL: string = 'http://localhost:3000';
+export const API_URL: string = false ? 'http://localhost:3000' : 'https://data.gieffektivt.no';
 export const API_AUTH: IApiAuth = {
   clientId: '1b43d77677588cfe9302fa729d1ab',
   permissions: [
