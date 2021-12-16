511e842a4bb738e8e7e12a429ca04360defcee72
Håkon Harnes <account@harnes.me>
2021-12-16 16:28:48 +0100

    Added ssn to add donor form

--- src/components/modules/donors/create/CreateDonor.style.tsx
+++ src/components/modules/donors/create/CreateDonor.style.tsx
@@ -4,8 +4,17 @@ export const CreateDonorWrapper = styled.div`
   display: flex;
   flex-direction: column;
   max-width: 200px;
+  padding: 20px;
 
   & > * {
     margin: 6px 0;
+  }
+
+  & > button {
+    justify-content: center;
+
+    svg {
+      margin-left: 6px;
+    }
   }
 `;
