194df89d16b594ed8db26db2d2ad16ae6ab33994
Håkon Harnes <account@harnes.me>
2022-01-13 19:58:58 +0100

    Moves allselected logic to effekt form component and removes uneeded state variables

--- src/components/style/elements/effekt-check/effekt-check-form.component.tsx
+++ src/components/style/elements/effekt-check/effekt-check-form.component.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { EffektCheckFormWrapper } from './effekt-check-form.component.style';
 import { EffektCheck } from './effekt-check.component';
 
@@ -12,13 +12,13 @@ interface IProps {
   choices: Array<EffektCheckChoice>;
   azure?: boolean;
   allChecked?: boolean;
-  onChange(value: number, checked: boolean): void;
+  onChange(selected: Array<number>): void;
 }
 
-export const EffektCheckForm: React.FunctionComponent<IProps> = ({ choices, azure, allChecked, onChange}) => {
+export const EffektCheckForm: React.FunctionComponent<IProps> = ({ choices, azure, onChange}) => {
   const azureColor: boolean = azure ? true : false;
- 
-  choices.push({label: "select all", value: 99, selected: allChecked} as EffektCheckChoice);
+
+  const isAllChecked 
 
   let checkBoxes = choices.map((choice, index) => (
     <EffektCheck
@@ -26,8 +26,13 @@ export const EffektCheckForm: React.FunctionComponent<IProps> = ({ choices, azur
       label={choice.label}
       checked={choice.selected}
       azure={azureColor}
-      onChange={(now_checked) => {onChange(choice.value, now_checked)}}
+      onChange={(now_checked) => {
+        choices.filter(choice => (choice.selected) && ! ).map(choice => choice.value)
+      }}
     ></EffektCheck>
   ));
-  return <EffektCheckFormWrapper>{checkBoxes}</EffektCheckFormWrapper>;
+  return <EffektCheckFormWrapper>
+    <span onClick={() => { onChange(choices.map(choice =>)) }}>Select all</span>
+    {checkBoxes}
+  </EffektCheckFormWrapper>;
 };
