868914da56ae0ef2e816a19aaf97a1b3223013e0
Herleik Holtan <hholtan@cisco.com>
2022-08-16 13:42:11 +0200

    adminpanel/donors: add total donation amount
    a table of total donations and total donations minus operations
    donations, in the same style as the key info table.
    
    resolves #184

--- src/components/pages/donors/Donor.style.tsx
+++ src/components/pages/donors/Donor.style.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const OverviewLine = styled.div`
   display: flex;
   margin-bottom: 40px;
-  max-width: 1024px;
+  max-width: 1324px;
   min-height: 245px;
   width: 100%;
   justify-content: space-between;
