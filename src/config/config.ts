a4fa71d26cc5e02091a8c45fe0cc6f230a0bcbf2
Lorentz Lasson <lasson.lorentz@gmail.com>
2023-04-25 19:33:31 +0200

    Allow configurable auth

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,7 @@
-export const DEV_ENVIRONMENT: boolean = process.env.NODE_ENV === 'development' ? true : false;
-export const API_URL: string = process.env.REACT_APP_API_URL || 'http://localhost:5050';
+export const DEV_ENVIRONMENT = process.env.NODE_ENV === 'development' ? true : false;
+export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5050';
 
+export const AUTH_DOMAIN = process.env.REACT_APP_AUTH_DOMAIN || '';
+export const AUTH_CLIENT_ID = process.env.REACT_APP_AUTH_CLIENT_ID || '';
+export const AUTH_AUDIENCE = process.env.REACT_APP_AUTH_AUDIENCE || '';
+export const AUTH_SCOPE = process.env.REACT_APP_AUTH_SCOPE || '';
