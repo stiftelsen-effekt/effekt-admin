29cc98c3feb58be1ab8566bb43dc01ea50d1b99e
Håkon Harnes <account@harnes.me>
2024-02-07 10:21:01 +0100

    Adds line controller to chartjs

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ import {
   Tooltip,
   PointElement,
   LineElement,
+  LineController,
 } from "chart.js";
 
 Chart.register(CategoryScale);
@@ -18,5 +19,6 @@ Chart.register(BarElement);
 Chart.register(Tooltip);
 Chart.register(PointElement);
 Chart.register(LineElement);
+Chart.register(LineController);
 
 ReactDOM.render(<App />, document.getElementById("root"));
